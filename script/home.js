cdf472f2fd50516cf0eb9382e0341754a78b68fe
mashiyyat15 <andriambolatsiory@gmail.com>
2022-05-07 09:03:31 +0300

    function

--- script/home.js
+++ script/home.js
@@ -20,6 +20,62 @@ function batslide(){
     else{
         i=0;
     }
-    setTimeout("batslide()", 2000);
+    setTimeout("batslide()", 3000); //défilement tous les 3s
 }
 window.onload = batslide;
+const ratio = 0.5
+//animation sur la page
+const options={
+    rootMargin:'0px',
+    threshold: 0.5
+}
+
+//FadeInh1
+const fadeIn = function (entries, observer){
+    entries.forEach(entry=>{
+        if (entry.intersectionRatio > ratio){
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target)
+        }
+    })
+}
+const observer  =new IntersectionObserver (fadeIn, options);
+document.querySelectorAll('h1').forEach(function(r){
+    observer.observe(r);
+})
+
+// fondu effect
+const fondu = function (entries, observeri){
+    entries.forEach(function(entry){
+        if (entry.intersectionRatio> ratio){
+            entry.target.classList.add(f_visible);
+            observeri.unobserve(entry.target);
+        }
+    })
+}
+const observei = new IntersectionObserver(fondu, options);
+document.querySelectorAll('.fondu').forEach(function(r){
+    observei.observe(r);
+})
+
+var cit=document.getElementsByClassName("cit")
+function diapo(){
+    let i = 0;
+    cit[i].style.display = "block";
+    for (let j=0; j<cit.length; j++){
+        if (j==i){
+            cit[j].style.display = "block";
+        }
+        else{
+            cit[j].style.display = "none";
+        }
+    }
+    if (i<cit.length-1){
+        i++;
+    }
+    else{
+        i = 0;
+    }
+    setTimeout("diapo()",3000)
+}
+window.onload = diapo();
